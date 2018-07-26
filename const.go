b7dd6d75798153edfa40e1ab1f3d10949bf8a031
qshuai <qishuai231@gmail.com>
2018-07-26 23:23:48 +0800

    coin: Update coin symbol BTC to BCH

--- const.go
+++ const.go
@@ -8,7 +8,7 @@ const (
 	// SatoshiPerBitcent is the number of satoshi in one bitcoin cent.
 	SatoshiPerBitcent = 1e6
 
-	// SatoshiPerBitcoin is the number of satoshi in one bitcoin (1 BTC).
+	// SatoshiPerBitcoin is the number of satoshi in one bitcoin (1 BCH).
 	SatoshiPerBitcoin = 1e8
 
 	// MaxSatoshi is the maximum transaction amount allowed in satoshi.
